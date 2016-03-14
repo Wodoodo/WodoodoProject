89ed69351818deb6180e1f431ac0c85a92e63ba9
wodooweb <vlad.yurhel@brokerbel.com>
2016-03-14 17:32:04 +0300

    Changed

---
+++ main/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<link rel="stylesheet" href="/main/stylesheet/header.css">
+	<link rel="stylesheet" href="/main/stylesheet/main.css">
+	<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+	<title>Wodoodo - Social network</title>
+</head>
+<body>
+	<?php
+		include $_SERVER['DOCUMENT_ROOT'].'/main/templates/header/header.tpl';
+	?>
+</body>
+</html>
