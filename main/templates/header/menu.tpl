77660580f8b4fd947e89227bc46b7317d4d91572
wodooweb <vlad.yurhel@brokerbel.com>
2016-03-17 22:22:05 +0300

    Edit header styles and added container

--- main/templates/header/menu.tpl
+++ main/templates/header/menu.tpl
@@ -1,6 +1,8 @@
 <div class="header_block service" style="z-index: 10">
-	<img src="./images/main/service_btn.png" id="services" alt="Service menu">
-	<div class="menu_services ">
+	<!--<img src="./images/main/service_btn.png" id="services" alt="Service menu">-->
+	<input type="checkbox" class="services" id="1">
+	<label for="1" class="service_button"></label>
+	<div class="menu_services">
 		<div class="inner_menu">
 			<div class="profile_avatar"><img src="https://pp.vk.me/c624720/v624720384/4b280/YW7IJWNn-yw.jpg"></div>
 			<p class="menu_name">Александр<br>Чуйко</p>
--- scripts/miniMenu.js
+++ scripts/miniMenu.js
@@ -1,40 +1,11 @@
 var isOpen = false;
-$(document).ready(function(){
-	$("#services").click(function(){
-		if (!isOpen){
-			$('.menu_services').animate({
-				"height" : 300
-			}, 300, function(){
-				$('.menu_services').animate({
-					"width" : 180
-				});
-				$('.user_basic_info').animate({
-					"margin-left":35
-				});
-				isOpen = true;
-			});
-		} else {
-			$('.user_basic_info').animate({
-				"margin-left":0
-			}, 350);
-			$('.menu_services').animate({
-				"width" : 35
-			}, 300, function(){
-				$('.menu_services').animate({
-					"height" : 0
-				});
-				isOpen = false;
-			});	
+$(document).mouseup(function(event){
+	if (event.target.className != 'service_button'){
+		if ($(".services").prop('checked')){
+			if ($(event.target).closest(".menu_services").length) return;
+			$(".menu_services").css('height', '0px');
+			event.stopPropafation;
+			$(".services").prop('checked', false);
 		}
-	});
-
-	var infoOpen = false;
-
-	/* USER INFO */
-	$('.user_info_btn').click(function(){
-		$('.user_info').css({
-			'visibility':'visible'
-		});
-		$('.user_info').toggle(300);
-	});
+	}
 });
